2ad35997cfa42197037426cf9afefb99cde91d9a
Wajdi-Mousa <wajdimousasoft@gmail.com>
2018-06-03 10:56:01 +0300

    test for create server

---
+++ mellanox_tempest_plugin/tests/scenario/test.py
@@ -0,0 +1,46 @@
+from tempest.scenario import manager
+from tempest import config
+
+from tempest.common import utils
+from tempest.common import waiters
+
+CONF = config.CONF
+
+
+class Test(manager.ScenarioTest):
+    credentials = ['primary', 'admin']
+
+    def setUp(self):
+        super(Test, self).setUp()
+        self.run_ssh = CONF.validation.run_validation
+        self.ssh_user = CONF.validation.image_ssh_user
+
+    def verify_ssh(self, keypair):
+        if self.run_ssh:
+            # Obtain a floating IP if floating_ips is enabled
+            if (CONF.network_feature_enabled.floating_ips and
+                    CONF.network.floating_network_name):
+                self.ip = self.create_floating_ip(self.instance)['ip']
+            else:
+                server = self.servers_client.show_server(
+                    self.instance['id'])['server']
+                self.ip = self.get_server_ip(server)
+            # Check ssh
+            self.ssh_client = self.get_remote_client(
+                ip_address=self.ip,
+                username=self.ssh_user,
+                private_key=keypair['private_key'],
+                server=self.instance)
+
+    @utils.services('compute', 'network')
+    def test_server(self):
+        keypair = self.create_keypair()
+        security_group = self._create_security_group()
+        self.instance = self.create_server(
+            key_name=keypair['name'],
+            security_groups=[{'name': security_group['name']}])
+        self.verify_ssh(keypair)
+
+    def test_wajdi(self):
+        name = "wajdi"
+        self.assertEqual(name , "wajdi")
